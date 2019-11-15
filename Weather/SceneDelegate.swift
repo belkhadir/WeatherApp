7de9b060fcd13120d1e863d5c1e90c8f622b74ba
Anas Belkhadir <belkhadir.anas@gmail.com>
2019-11-15 19:03:29 +0100

    Fixing bugs: each time the app was open it's save duplicate data

--- Weather/SceneDelegate.swift
+++ Weather/SceneDelegate.swift
@@ -18,7 +18,13 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
         // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
         if let windowScene = scene as? UIWindowScene {
-            addDefaultCityToCoreData()
+            
+            if !isFirstTimeOpen {
+                addDefaultCityToCoreData()
+                UserDefaults.standard.set(true, forKey: "isFirstTimeOpen")
+            }
+            
+            
             let window = UIWindow(windowScene: windowScene)
             let cityTableViewController = UINavigationController(rootViewController: CityTableViewController(style: .grouped))
             window.rootViewController = cityTableViewController
@@ -29,34 +35,8 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
     }
 
-    func sceneDidDisconnect(_ scene: UIScene) {
-        // Called as the scene is being released by the system.
-        // This occurs shortly after the scene enters the background, or when its session is discarded.
-        // Release any resources associated with this scene that can be re-created the next time the scene connects.
-        // The scene may re-connect later, as its session was not neccessarily discarded (see `application:didDiscardSceneSessions` instead).
+    var isFirstTimeOpen: Bool {
+        return UserDefaults.standard.bool(forKey: "isFirstTimeOpen")
     }
-
-    func sceneDidBecomeActive(_ scene: UIScene) {
-        // Called when the scene has moved from an inactive state to an active state.
-        // Use this method to restart any tasks that were paused (or not yet started) when the scene was inactive.
-    }
-
-    func sceneWillResignActive(_ scene: UIScene) {
-        // Called when the scene will move from an active state to an inactive state.
-        // This may occur due to temporary interruptions (ex. an incoming phone call).
-    }
-
-    func sceneWillEnterForeground(_ scene: UIScene) {
-        // Called as the scene transitions from the background to the foreground.
-        // Use this method to undo the changes made on entering the background.
-    }
-
-    func sceneDidEnterBackground(_ scene: UIScene) {
-        // Called as the scene transitions from the foreground to the background.
-        // Use this method to save data, release shared resources, and store enough scene-specific state information
-        // to restore the scene back to its current state.
-    }
-
-
 }
 
