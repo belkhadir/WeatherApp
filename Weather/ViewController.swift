e906bd2006e2d69bd775c9311337f64210fa10a7
Anas Belkhadir <belkhadir.anas@gmail.com>
2019-11-15 10:46:25 +0100

    The goal of the app is to To help people plan for the future and avoid catching cold

---
+++ Weather/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Weather
+//
+//  Created by swiftios01 on 15/11/2019.
+//  Copyright © 2019 Babel. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
